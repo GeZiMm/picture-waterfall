9c6fa194a5da182e332021498a806161950e2f6e
zhoupei <zhoupei@tuhu.cn>
2019-09-18 16:00:44 +0800

    基于vue-cli3+vue-waterfall-easy实现图片瀑布流代码

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <!-- 瀑布流组件 -->
+        <PictureWaterfall/>
+    </div>
+</template>
+
+<script>
+    import PictureWaterfall from "./components/PictureWaterfall.vue";
+
+    export default {
+        name: "app",
+        components: {
+            PictureWaterfall
+        },
+        data() {
+            return {};
+        }
+    };
+</script>
+
+<style>
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/PictureWaterfall.vue
@@ -0,0 +1,39 @@
+<template>
+<!--    参考https://github.com/lfyfly/vue-waterfall-easy/blob/master/README-CN.md-->
+    <vueWaterfallEasy ref="waterfall"
+                      :imgsArr="imgArr"
+                      :height="200"
+                      :gap="20"
+                      @scrollReachBottom="getData"
+    >
+        <div slot="waterfall-over">已经到底了</div>
+    </vueWaterfallEasy>
+</template>
+
+<script>
+    import VueWaterfallEasy from "vue-waterfall-easy";
+
+    export default {
+        name: "PictureWaterfall",
+        components: {VueWaterfallEasy},
+        data() {
+            return {
+                imgArr: [{src: require('../assets/1.jpg'), href: ''}, {src: require('../assets/3.jpg'), href: ''}],
+                count: 2,
+            };
+        },
+        methods: {
+            getData() {
+                if (this.count === 20) {
+                    this.$refs.waterfall.waterfallOver()
+                } else {
+                    this.count += 1;
+                    this.imgArr.push({src: require('../assets/' + this.count + '.jpg'), href: ''})
+                }
+            }
+        }
+    };
+</script>
+
+<style scoped>
+</style>
